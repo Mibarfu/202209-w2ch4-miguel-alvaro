c824873751f6b2d3230efb9d4c6955d6949c1a0e
Miguel Ángel Barquilla <miguelangelbarquilla@gmail.com>
2022-10-06 20:51:14 +0200

    Install eslint and hasky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,21 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true
-  },
-  extends: ["airbnb-base", "prettier"],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  rules: {
-    "lines-between-class-members": [
-      "error",
-      "always",
-      { exceptAfterSingleLine: true },
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
     ],
-  },
-};
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,3 @@
-import SkylabArray from "./SkylabArray";
+import SkylabArray from "./SkylabArray.js";
 
 let skyLab = new SkylabArray(1, 2, 4, 6, 5, 3);
