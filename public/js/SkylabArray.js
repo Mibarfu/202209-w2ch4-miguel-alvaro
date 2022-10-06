8b69531e78426a2bf5f334efb2835ef04302bf08
Miguel Ángel Barquilla <miguelangelbarquilla@gmail.com>
2022-10-06 20:07:57 +0200

    Create class body

---
+++ public/js/SkylabArray.js
@@ -0,0 +1,11 @@
+class SkylabArray {
+  arraySkilab;
+
+  constructor(...values) {
+    this.arraySkilab = values;
+  }
+}
+
+let skylabArray = new SkylabArray(2, 4, 5, 8);
+
+console.log(skylabArray);
